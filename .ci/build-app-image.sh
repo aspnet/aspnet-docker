fb4aa2cc0212d0f26785d716f6748f1572edf566
Vincent Lesierse <vincent@lesierse.com>
2015-02-22 22:48:41 +0100

    React to changes kvm.sh for nightly an other versions
    - Use KVM_USER_HOME instead of KRE_USER_HOME
    - Change folder from packaged to runtimes

--- .ci/build-app-image.sh
+++ .ci/build-app-image.sh
@@ -4,13 +4,17 @@ set -e 	# Exit immediately upon failure
 : ${1?"Need to pass BASE_IMAGE as argument"}
 : ${2?"Need to pass TEST_APP as argument"}
 : ${3?"Need to pass TAG as argument"}
+: ${4?"Need to pass BRANCH as argument"}
 
 BASE_IMAGE=$1
 TEST_APP=$2
 TAG=$3
+BRANCH=$4
 
 echo "[CI] Injecting Dockerfile to project $TEST_APP..."
 cd  $SAMPLES_REPO/samples/$TEST_APP
+echo "[CI] Checkout branch $BRANCH"
+git checkout $BRANCH
 tee Dockerfile << EOF
 FROM $BASE_IMAGE
 COPY . /app
@@ -23,3 +27,5 @@ EOF
 echo "[CI] Building Docker image for $TEST_APP, will tag as '$TAG'..."
 docker build -t $TAG .
 echo "[CI] Built Docker image '$TAG'"
+echo "[CI] Revert checkout"
+git checkout master
--- .ci/run-app.sh
+++ .ci/run-app.sh
@@ -3,15 +3,17 @@ set -e 	# Exit immediately upon failure
 
 : ${1?"Need to pass BASE_IMAGE as argument"}
 : ${2?"Need to pass TEST_APP as argument"}
+: ${3?"Need to pass BRANCH as argument"}
 
 BASE_IMAGE=$1
 TEST_APP=$2
 TEST_PORT=$RANDOM
+BRANCH=$3
 
 APP_IMG_TAG=$(tr '[:upper:]' '[:lower:]' <<< $TEST_APP)_${TEST_PORT}
 
 # Build the app image
-.ci/build-app-image.sh $BASE_IMAGE $TEST_APP $APP_IMG_TAG
+.ci/build-app-image.sh $BASE_IMAGE $TEST_APP $APP_IMG_TAG $BRANCH
 
 # Start app
 .ci/start-container.sh 5004 $TEST_PORT $APP_IMG_TAG $APP_IMG_TAG
