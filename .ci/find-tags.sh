facdeaedfdb170832ef0d4935d3d079a0dfcf3cd
Troy Dai <troy.dai@outlook.com>
2016-09-20 14:58:16 -0700

    Remove circleci

--- .ci/find-tags.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e 	# Exit immediately upon failure
-set -o pipefail # carry failures over pipes
-
-: ${1?"Need to pass Dockerfile search directory as argument"}
-
-cd $1
-find . -path ./.git -prune -o -name Dockerfile -print0 | xargs -0 -n1 dirname | sed -e "s/\.\///" | grep -v samples
--- .ci/test-connection.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e 	# Exit immediately upon failure
-
-: ${1?"Need to pass URL"}
-URL=$1
-
-curl -vsSLI --retry 10 --retry-delay 3 $URL
