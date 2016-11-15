4813d8210214953656168e643c1a9248017f2797
Nate McMaster <nate.mcmaster@microsoft.com>
2016-11-15 11:40:13 -0800

    Workaround incompatibility of preview2 and preview3 CLI in expected package cache layout

---
+++ 1.1.0/jessie/runtime/build-cache.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+set -e
+
+curl -sSLo /tmp/packagescache.tar.gz \
+    https://distaspnet.blob.core.windows.net/packagecache/1.1.0/aspnetcore.packagecache-1.1.0-legacy-debian.8-x64.tar.gz
+mkdir -p $DOTNET_HOSTING_OPTIMIZATION_CACHE
+tar xf /tmp/packagescache.tar.gz -C $DOTNET_HOSTING_OPTIMIZATION_CACHE
+rm /tmp/packagescache.tar.gz
+
+link_lowercase() {
+    filename=$(basename $1)
+    dir=$(dirname $1)
+    lower_filename=$(echo $filename | awk '{print tolower($0)}')
+    ln -s $1 $dir/$lower_filename
+    echo "Created symlink '$1' => '$dir/$lower_filename'"
+}
+
+# create lower case symlinks because NuGet 3.5 (project.json) and NuGet 4 (MSBuild)
+# results in different casing in deps.json which affects cache lookup
+
+for shafile in $DOTNET_HOSTING_OPTIMIZATION_CACHE/x64/*/*/*.sha512; do
+    link_lowercase $shafile
+done
+
+for dir in $DOTNET_HOSTING_OPTIMIZATION_CACHE/x64/*; do
+    link_lowercase $dir
+done
