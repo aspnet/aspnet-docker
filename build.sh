1510e82012dffee60f257b777c024351f214041a
glennc <glennc@microsoft.com>
2017-01-29 22:17:55 -0800

    initial test code and travis configuration

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e 	# Exit immediately upon failure
+set -o pipefail  # Carry failures over pipes
+
+repo_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+docker_repo="microsoft/aspnetcore"
+
+function build_dockerfiles {
+    for dockerfile_dir in ${1}; do
+        echo "----- ${dockerfile_dir} -----"
+        tag="${docker_repo}:$( sed -e 's/.\///' -e 's/jessie\///' -e 's/\//-/g' <<< "${dockerfile_dir}" )"
+        echo "----- Building ${tag} -----"
+        docker build -t "${tag}" "${dockerfile_dir}"
+    done
+}
+
+build_dockerfiles "$( find . -path './.*' -prune -o -name 'Dockerfile' -a -path '**/*' -print0 | xargs -0 -n1 dirname )"
