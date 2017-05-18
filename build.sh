8a753546da8805af4a5eb0ddaa597dd53c5a4cad
MichaelSimons <msimons@microsoft.com>
2017-05-18 16:29:18 -0500

    Update build.sh to use correct repo name per image type

--- build.sh
+++ build.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
-set -e 	# Exit immediately upon failure
+set -e  # Exit immediately upon failure
 set -o pipefail  # Carry failures over pipes
 
 repo_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker_repo="test/aspnetcore"
 
 function build_dockerfiles {
     for dockerfile_dir in ${1}; do
         echo "----- ${dockerfile_dir} -----"
-        tag="${docker_repo}:$( sed -e 's/.\///' -e 's/jessie\///' -e 's/\//-/g' <<< "${dockerfile_dir}" )"
-        echo "----- Building ${tag} -----"
-        docker build --pull -t "${tag}" "${dockerfile_dir}"
+        tag="$( sed -e 's/.\///' -e 's/jessie\///' -e 's/\//-/g' <<< "${dockerfile_dir}" )"
+        case $tag in
+            *-runtime )
+                docker_repo="test/aspnetcore"
+                ;;
+            *-sdk|*-kitchensink )
+                docker_repo="test/aspnetcore-build"
+                ;;
+        esac
+        image_name="$docker_repo:$tag"
+        echo "----- Building ${image_name} -----"
+        docker build --pull -t "${image_name}" "${dockerfile_dir}"
     done
 }
 
