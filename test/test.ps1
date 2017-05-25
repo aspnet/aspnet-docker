b938d92d0af56f16d86e1abb52dfada6bc43a813
Michael Simons <msimons@microsoft.com>
2017-05-25 16:02:22 -0500

    Create single cross plat test script (#240)

--- test/test.ps1
+++ test/test.ps1
@@ -1,8 +1,11 @@
 param(
     # Set to 'microsoft' on build servers
-    [string]$RootImageName='test'
+    [string]$RootImageName='test',
+    # Set to Docker host IP if running within a container
+    [string]$HostIP='localhost'
 )
 
+Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
 # Functions
@@ -20,87 +23,151 @@ function fatal {
     exit 1
 }
 
-function normalize-slashes($str) {
-    $str -replace '\\', '/'
+function Get-Ip($container, $platform) {
+    if ($platform -eq "windows") {
+        docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container
+    }
+    else {
+        $HostIP
+    }
 }
 
-function get-ip($container) {
-    docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container
+function Join-Paths($path, $childPaths) {
+    $childPaths | %{ $path = Join-Path $path $_ }
+    return $path
 }
 
 function WaitForSuccess($endpoint) {
     # wait 1 minutes max
     for ($i = 0; $i -lt 15; $i++) {
-        write-host -f gray "Waiting for $endpoint"
+        Write-Host -f gray "Waiting for $endpoint"
         try {
-            iwr -UseBasicParsing $endpoint
+            Invoke-WebRequest -UseBasicParsing $endpoint
             return 0
         }
         catch {
-            write-host -f gray 'Still waiting'
-            sleep -Seconds 4
+            Write-Host -f gray 'Still waiting'
+            Start-Sleep -s 4
         }
     }
     fatal "Timed out waiting for response on $endpoint"
 }
 
 # Main
-$repo_root = normalize-slashes "$PSScriptRoot/.."
+$platform = docker version -f "{{ .Server.Os }}"
+if ($platform -eq "windows") {
+    $container_root = "C:\"
+    $host_port = "80"
+    $image_os = "nanoserver"
+    $rid="win7-x64"
+}
+else {
+    $container_root = "/"
+    $host_port = "5000"
+    $image_os = "jessie"
+    $rid="debian.8-x64"
+}
 
-gci $PSScriptRoot/../*/nanoserver/sdk/Dockerfile | % {
+Get-ChildItem (Join-Paths $PSScriptRoot ("..", "*", $image_os, "sdk", "Dockerfile")) | % {
 
     $app_name = "app$(get-random)"
-    $app_dir = "${repo_root}/.test-assets/${app_name}"
-    mkdir $app_dir -ErrorAction Ignore | Out-Null
-
-    echo "---- Generating application directory ${app_dir} ---- "
-
+    $publish_path = "${container_root}publish"
     $version = $_.Directory.Parent.Parent.Name
-    $framework = "netcoreapp${version}"
     $sdk_tag = "$RootImageName/aspnetcore-build:${version}"
     $runtime_tag = "$RootImageName/aspnetcore:${version}"
 
-    echo "---- Testing ${sdk_tag} and ${runtime_tag} ----"
-    exec docker run -t `
-        -v "${app_dir}:C:/${app_name}" `
-        -v "${repo_root}/test:C:/test" `
-        --name "build-test-${app_name}" `
-        --entrypoint powershell `
-        $sdk_tag `
-        -Command "C:/test/create-run-publish-app.ps1 C:/$app_name $framework"
+    Write-Host "----- Building app with ${sdk_tag} -----"
+    if ($version -eq '1.0' -or $version -eq '1.1') {
+        $optional_new_params = ""
+    } else {
+        $optional_new_params = "--no-restore"
+    }
 
-    echo "----- Testing ${runtime_tag} with ${sdk_tag} app -----"
+    $app_build_tag = "$app_name-build"
     try {
-        exec docker run -d -t `
-            -v "${app_dir}:C:/${app_name}" `
-            --workdir "C:/${app_name}" `
-            --name "runtime-test-${app_name}" `
-            -p 80:80 `
-            --entrypoint dotnet `
-            $runtime_tag `
-            "C:/${app_name}/publish/framework-dependent/${app_name}.dll"
+        $framework ="netcoreapp${version}"
+        exec {
+            (Get-Content (Join-Path $PSScriptRoot -ChildPath "Dockerfile.test.$image_os")).
+                Replace("{image}", $sdk_tag) `
+            | docker build `
+                --build-arg FRAMEWORK=$framework `
+                --build-arg OPTIONAL_NEW_PARAMS=$optional_new_params `
+                -t $app_build_tag `
+                -
+        }
 
-        $ip = get-ip "runtime-test-${app_name}"
-        WaitForSuccess "http://${ip}:80"
+        Write-Host "----- Publishing framework-dependent app with ${sdk_tag} -----"
+        $app_volume_name = "$app_name-framework-dependent"
+        try {
+            exec docker run --rm `
+                --name "publish-framework-dependent-$app_name" `
+                -v ${app_volume_name}:${publish_path} `
+                $app_build_tag `
+                dotnet publish --configuration Release --output $publish_path
+
+            Write-Host "----- Running framework-dependent app with ${runtime_tag} -----"
+            $app_container_name = "runtime-framework-dependent-${app_name}"
+            try {
+                exec docker run -d -t `
+                    --entrypoint dotnet `
+                    --name $app_container_name `
+                    -p ${host_port}:80 `
+                    -v ${app_volume_name}:${publish_path} `
+                    --workdir ${publish_path} `
+                    $runtime_tag `
+                    test.dll
+
+                $ip = Get-Ip $app_container_name $platform
+                WaitForSuccess "http://${ip}:${host_port}"
+            }
+            finally {
+                exec docker rm -f $app_container_name
+            }
+        }
+        finally {
+            exec docker volume rm $app_volume_name
+        }
+
+        Write-Host "----- Publishing self-contained app with ${sdk_tag} -----"
+        $app_volume_name = "$app_name-self-contained"
+        try {
+            exec docker run --rm `
+                --name "publish-self-contained-$app_name" `
+                -v ${app_volume_name}:${publish_path} `
+                $app_build_tag `
+                dotnet publish --configuration Release --runtime $rid --output $publish_path
+
+            if ($platform -eq "linux" -and $version -eq "2.0") {
+                # Temporary workaround https://github.com/dotnet/corefx/blob/master/Documentation/project-docs/dogfooding.md#option-2-self-contained
+                exec docker run --rm `
+                    -v ${app_volume_name}:${publish_path} `
+                    $runtime_tag `
+                    chmod u+x ${publish_path}/test
+            }
+
+            Write-Host "----- Running self-contained app with ${runtime_tag} -----"
+            $app_container_name = "runtime-self-contained-${app_name}"
+            try {
+                exec docker run -d -t `
+                    --entrypoint ./test `
+                    --name $app_container_name `
+                    -p ${host_port}:80 `
+                    -v ${app_volume_name}:${publish_path} `
+                    --workdir ${publish_path} `
+                    $runtime_tag
+
+                $ip = Get-Ip $app_container_name $platform
+                WaitForSuccess "http://${ip}:${host_port}"
+            }
+            finally {
+                exec docker rm -f $app_container_name
+            }
+        }
+        finally {
+            exec docker volume rm $app_volume_name
+        }
     }
     finally {
-        exec docker rm -f "runtime-test-${app_name}"
-    }
-
-    echo "----- Testing ${runtime_tag} with standalone ${sdk_tag} app -----"
-    try {
-        exec docker run -d -t `
-            -v "${app_dir}:C:/${app_name}" `
-            --workdir "C:/${app_name}" `
-            --name "runtime-standalone-test-${app_name}" `
-            -p 80:80 `
-            --entrypoint "C:/${app_name}/publish/self-contained/${app_name}" `
-            $runtime_tag
-
-        $ip = get-ip "runtime-standalone-test-${app_name}"
-        WaitForSuccess "http://${ip}:80"
-    }
-    finally {
-        exec docker rm -f "runtime-standalone-test-${app_name}"
+        exec docker rmi -f $app_build_tag
     }
 }
