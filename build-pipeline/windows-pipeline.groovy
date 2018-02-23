99bf1dd3f1365e0dd16e4581d09cb534ef0d7cd7
Nate McMaster <nate.mcmaster@microsoft.com>
2018-02-22 16:05:39 -0800

    Setup dotnet-ci for this repo

--- build-pipeline/windows-pipeline.groovy
+++ build-pipeline/windows-pipeline.groovy
@@ -1,8 +1,17 @@
 @Library('dotnet-ci') _
 
 def buildImages = {
-    stage ('Stub') {
-        bat 'echo Hello world'
+    stage ('Checking out source') {
+        checkout scm
+    }
+    stage ('Cleanup Docker') {
+        bat 'docker system prune -a -f'
+    }
+    stage ('Build and test images') {
+        bat "powershell -NoProfile -Command .\\ci-build.ps1 -Folder \"${params.folderFilter}\""
+    }
+    stage ('Cleanup') {
+        bat 'docker system prune -a -f'
     }
 }
 
--- ci-build.sh
+++ ci-build.sh
@@ -2,7 +2,23 @@
 
 set -euo pipefail
 
+folder_filter=''
 [ -z "${folder_filter:-}" ] && folder_filter='*'
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --folder-filter)
+            shift
+            [ -z "${1+x}" ] && echo "Missing value for --folder-filter" && exit 1
+            folder_filter="$1"
+            ;;
+        *)
+            echo "Unrecognized argument $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
 
 if [ "$(uname)" = "Darwin" ]; then
     host_ip="$(ifconfig en0 | awk '$1 == "inet" {print $2}')"
