42087d428ab8bb8d874b0885da29bc5178022aec
Nate McMaster <nate.mcmaster@microsoft.com>
2018-03-06 10:05:51 -0800

    Add names to pipeline triggers

--- build-pipeline/pipelinejobs.groovy
+++ build-pipeline/pipelinejobs.groovy
@@ -4,19 +4,25 @@ def windowsPipeline = Pipeline.createPipeline(this, 'windows-pipeline.groovy')
 
 // Images that can only build on RS1, not RS3
 ['1.*', '2.1/nanoserver-sac2016/*', '2.1/nanoserver-sac2016/*'].each { folderFilter ->
-    windowsPipeline.triggerPipelineOnEveryGithubPR(['folderFilter':folderFilter])
-    windowsPipeline.triggerPipelineOnGithubPush(['folderFilter':folderFilter])
+    def triggerName = "Windows SAC2016 ${folderFilter[0..-2]} Build"
+
+    windowsPipeline.triggerPipelineOnEveryGithubPR(triggerName, ['folderFilter':folderFilter])
+    windowsPipeline.triggerPipelineOnPush(triggerName, ['folderFilter':folderFilter])
 }
 
 // Images that can only build on RS3, not RS1
 ['2.1/nanoserver-1709/*', '2.1/nanoserver-1709/*'].each { folderFilter ->
-    windowsPipeline.triggerPipelineOnEveryGithubPR(['RS3':true, 'folderFilter':folderFilter])
-    windowsPipeline.triggerPipelineOnGithubPush(['RS3':true, 'folderFilter':folderFilter])
+    def triggerName = "Windows 1709 ${folderFilter[0..-2]} Build"
+
+    windowsPipeline.triggerPipelineOnEveryGithubPR(triggerName, ['RS3':true, 'folderFilter':folderFilter])
+    windowsPipeline.triggerPipelineOnPush(triggerName, ['RS3':true, 'folderFilter':folderFilter])
 }
 
 def linuxPipeline = Pipeline.createPipeline(this, 'linux-pipeline.groovy')
 
 ['1.*', '2.0/*', '2.1/*'].each { folderFilter ->
-    linuxPipeline.triggerPipelineOnEveryGithubPR(['folderFilter':folderFilter])
-    linuxPipeline.triggerPipelineOnGithubPush(['folderFilter':folderFilter])
+    def triggerName = "Linux ${folderFilter[0..-2]} Build"
+
+    linuxPipeline.triggerPipelineOnEveryGithubPR(triggerName, ['folderFilter':folderFilter])
+    linuxPipeline.triggerPipelineOnPush(triggerName, ['folderFilter':folderFilter])
 }
